a99013041553558210bfda8b599e0f0863e61c51
kempalshik <55582790+kempalshik@users.noreply.github.com>
2021-02-03 00:47:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -2,70 +2,45 @@
     require('header.php');
 ?>
 
-<main>
 <!-- Preload! für die Bilder  -->
-<div id="preloader">
-    
-    <!-- Programmkioersprachen Pics -->
-    <img src="img/html.jpg" alt="">
-    <img src="img/css.jpg" alt="">
-    <img src="img/javascript.jpg" alt="">
-    <img src="img/php.jpg" alt="">
-    <img src="img/csharp.jpg" alt="">
-    <img src="img/java.jpg" alt="">
-    <img src="img/c++.jpg" alt="">
-    <img src="img/solidity.jpg" alt="">
-    <img src="img/keyboard.jpg" alt="">
+    <div id="preloader">
+        <!-- Programmkioersprachen Pics -->
+        <img src="img/html.jpg" alt="">
+        <img src="img/css.jpg" alt="">
+        <img src="img/javascript.jpg" alt="">
+        <img src="img/php.jpg" alt="">
+        <img src="img/csharp.jpg" alt="">
+        <img src="img/java.jpg" alt="">
+        <img src="img/c++.jpg" alt="">
+        <img src="img/solidity.jpg" alt="">
+        <img src="img/keyboard.jpg" alt="">
 
-    <!-- main Pics Download -->
-    <img src="img/skyline.jpg" alt="">
-    <img src="img/buble.jpg" alt="">
-    <img src="img/winter.jpg" alt="">
-    <img src="img/sunset.jpg" alt="">
-    <img src="img/satelite.jpg" alt="">
-    <img src="img/sea.jpg" alt="">
-    <img src="img/trees.jpg" alt="">
-    <img src="img/pyrenees.jpg" alt="">
-    <img src="img/dive.jpg" alt="">
-    <img src="img/forest.jpg" alt="">
-    <img src="img/mountain.jpg" alt="">
+        <!-- main Pics Download -->
+        <img src="img/skyline.jpg" alt="">
+        <img src="img/buble.jpg" alt="">
+        <img src="img/winter.jpg" alt="">
+        <img src="img/sunset.jpg" alt="">
+        <img src="img/satelite.jpg" alt="">
+        <img src="img/sea.jpg" alt="">
+        <img src="img/trees.jpg" alt="">
+        <img src="img/pyrenees.jpg" alt="">
+        <img src="img/dive.jpg" alt="">
+        <img src="img/forest.jpg" alt="">
+    </div>
 
-    <!-- main Pics Eigene -->
-    <!-- <img src="img/berlin.jpg" alt="">
-    <img src="img/brisat.jpg" alt="">
-    <img src="img/flugbahn.jpg" alt="">
-    <img src="img/h1.jpg" alt="">
-    <img src="img/hafen.jpg" alt="">
-    <img src="img/hotel.jpg" alt="">
-    <img src="img/koeln.jpg" alt="">
-    <img src="img/mship.jpg" alt="">
-    <img src="img/tonisee.jpg" alt=""> -->
-    
 
-    <!-- <img src="img/bau.jpg" alt="">
-    <img src="img/mc2.jpg" alt="">
-    <img src="img/barcelonapalm.jpg" alt="">
-    <img src="img/mc1.jpg" alt="">
-    <img src="img/land2.jpg" alt="">
-    <img src="img/flug.jpg" alt="">
-    <img src="img/msonne.jpg" alt="">
-    <img src="img/p1.jpg" alt="">
-    <img src="img/palmb.jpg" alt="">
-    <img src="img/a380.jpg" alt="">
-    <img src="img/bturm.jpg" alt="">
-    <img src="img/riverice.jpg" alt="">
-    <img src="img/skyflug.jpg" alt="">
-    <img src="img/view.jpg" alt="">
-    <img src="img/trptower.jpg" alt=""> -->
-</div>
 
-    <section class="main-wrapper-img"></section> <!-- bild unter dem nav -->
+<main>
+
+    <section class="main-wrapper-img">
+        <h2 class="imgWelcome">Wilkommen auf meinem Portfolio</h2> 
+    </section> <!-- bild unter dem nav -->
     <!-- main text -->
-    <section class="main-wrapper-text">
-        <h2 class="welcome">Wilkommen auf meiner Bewerbungsmappe</h2> 
-        <h4 class="welcome-autotype">Hier können Sie sich 
+    <section class="typing-wrapper-text">
+        <h2 class="welcome">Hier können Sie sich</h2> 
+        <div class="welcome-autotype">sehr gerne
         <span class="txt-type" data-wait="3000" data-words='["meine Bewerungsunterlagen einsehen.", "einen Überblick über meine Erfahrung und meine Projekte verschaffen."]'></span>
-        </h4>
+    </div>
     </section>
     <!-- main cards -->
     
---
+++ picpreloader.php
@@ -0,0 +1,97 @@
+<div id="preloader">
+    
+    <!-- Programmkioersprachen Pics -->
+    <img src="img/html.jpg" alt="">
+    <img src="img/css.jpg" alt="">
+    <img src="img/javascript.jpg" alt="">
+    <img src="img/php.jpg" alt="">
+    <img src="img/csharp.jpg" alt="">
+    <img src="img/java.jpg" alt="">
+    <img src="img/c++.jpg" alt="">
+    <img src="img/solidity.jpg" alt="">
+    <img src="img/keyboard.jpg" alt="">
+
+    <!-- main Pics Download -->
+    <img src="img/skyline.jpg" alt="">
+    <img src="img/buble.jpg" alt="">
+    <img src="img/winter.jpg" alt="">
+    <img src="img/sunset.jpg" alt="">
+    <img src="img/satelite.jpg" alt="">
+    <img src="img/sea.jpg" alt="">
+    <img src="img/trees.jpg" alt="">
+    <img src="img/pyrenees.jpg" alt="">
+    <img src="img/dive.jpg" alt="">
+    <img src="img/forest.jpg" alt="">
+    <!-- <img src="img/mountain.jpg" alt=""> -->
+    <!-- <img src="img/seebaum.jpg" alt=""> -->
+
+    <!-- main Pics Eigene -->
+    <!-- <img src="img/berlin.jpg" alt="">
+    <img src="img/brisat.jpg" alt="">
+    <img src="img/flugbahn.jpg" alt="">
+    <img src="img/h1.jpg" alt="">
+    <img src="img/hafen.jpg" alt="">
+    <img src="img/hotel.jpg" alt="">
+    <img src="img/koeln.jpg" alt="">
+    <img src="img/mship.jpg" alt="">
+    <img src="img/tonisee.jpg" alt=""> -->
+    
+
+    <!-- <img src="img/bau.jpg" alt="">
+    <img src="img/mc2.jpg" alt="">
+    <img src="img/barcelonapalm.jpg" alt="">
+    <img src="img/mc1.jpg" alt="">
+    <img src="img/land2.jpg" alt="">
+    <img src="img/flug.jpg" alt="">
+    <img src="img/msonne.jpg" alt="">
+    <img src="img/p1.jpg" alt="">
+    <img src="img/palmb.jpg" alt="">
+    <img src="img/a380.jpg" alt="">
+    <img src="img/bturm.jpg" alt="">
+    <img src="img/riverice.jpg" alt="">
+    <img src="img/skyflug.jpg" alt="">
+    <img src="img/view.jpg" alt="">
+    <img src="img/trptower.jpg" alt=""> -->
+</div>
+
+
+<!-- für css -->
+
+
+<!-- 
+/* @keyframes animate_img {
+    0% {
+        background-image: url("img/palmb.jpg");
+    }
+    10% {
+        background-image: url("img/brisat.jpg");
+    }
+    20% {
+        background-image: url("img/berlin.jpg");
+    }
+    30% {
+        background-image: url("img/tonisee.jpg");
+    }
+    40% {
+        background-image: url("img/mship.jpg");
+    }
+    50% {
+        background-image: url("img/flugbahn.jpg");
+    }
+    60% {
+        background-image: url("img/h1.jpg");
+    }
+    70% {
+        background-image: url("img/hafen.jpg");
+    }
+    80% {
+        background-image: url("img/hotel.jpg");
+    }
+    90% {
+        background-image: url("img/koeln.jpg");
+    }
+    100% {
+        background-image: url("img/palmb.jpg");
+    }
+}
+ */ -->
